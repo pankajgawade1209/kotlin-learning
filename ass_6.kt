a18dc99bc81e3c49bf4638767f4b9d1845da4c85
pankajgawade1209 <pankajgawade9299@gmail.com>
2025-08-22 14:16:43 +0530

    access it via sub class

---
+++ ass_6.kt
@@ -0,0 +1,21 @@
+open class BankAccount(
+    val accountHolder: String,
+    internal val bankName: String,
+    private val balance: Double
+) {
+    protected fun showBalance() {
+        println("Account Holder: $accountHolder")
+        println("Bank Name: $bankName")
+        println("Current Balance: $balance")
+    }
+}
+
+class SavingsAccount(
+    accountHolder: String,
+    bankName: String,
+    balance: Double
+) : BankAccount(accountHolder, bankName, balance) {
+
+    fun displayAccountDetails() {
+        showBalance()
+    }
